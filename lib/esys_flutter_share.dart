a2989e41cd94afce5246d82836e11e66e7ee71a7
Krzysztof Hycnar <khycnar@o2.pl>
2020-05-31 00:08:41 +0100

    iPad fix (rect)

--- lib/esys_flutter_share.dart
+++ lib/esys_flutter_share.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:io';
+import 'dart:ui';
 
 import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
@@ -10,18 +11,20 @@ class Share {
       'channel:github.com/orgs/esysberlin/esys-flutter-share');
 
   /// Sends a text to other apps.
-  static void text(String title, String text, String mimeType) {
-    Map argsMap = <String, String>{
+  static void text(String title, String text, String mimeType,
+      {Rect sharePositionOrigin}) {
+    Map argsMap = <String, dynamic>{
       'title': '$title',
       'text': '$text',
       'mimeType': '$mimeType'
     };
+    argsMap = _addRect(argsMap, sharePositionOrigin);
     _channel.invokeMethod('text', argsMap);
   }
 
   /// Sends a file to other apps.
   static Future<void> file(
-      String title, String name, List<int> bytes, String mimeType, {String text = '', String subject = ''}) async {
+      String title, String name, List<int> bytes, String mimeType, {String text = '', String subject = '', Rect sharePositionOrigin}) async {
     Map argsMap = <String, String>{
       'title': '$title',
       'name': '$name',
@@ -33,13 +36,14 @@ class Share {
     final tempDir = await getTemporaryDirectory();
     final file = await new File(join(tempDir.path, name)).create();
     await file.writeAsBytes(bytes);
-
+    argsMap = _addRect(argsMap, sharePositionOrigin);
     _channel.invokeMethod('file', argsMap);
   }
 
   /// Sends multiple files to other apps.
   static Future<void> files(
-      String title, Map<String, List<int>> files, String mimeType, {String text = ''}) async {
+      String title, Map<String, List<int>> files, String mimeType,
+      {String text = '', Rect sharePositionOrigin}) async {
     Map argsMap = <String, dynamic>{
       'title': '$title',
       'names': files.entries.toList().map((x) => x.key).toList(),
@@ -54,6 +58,20 @@ class Share {
       await file.writeAsBytes(entry.value);
     }
 
+    argsMap = _addRect(argsMap, sharePositionOrigin);
+
     _channel.invokeMethod('files', argsMap);
+  }
+
+  static Map<String, dynamic> _addRect(
+      Map<String, dynamic> argsMap, Rect sharePositionOrigin) {
+    if (sharePositionOrigin != null) {
+      argsMap['originX'] = sharePositionOrigin.left;
+      argsMap['originY'] = sharePositionOrigin.top;
+      argsMap['originWidth'] = sharePositionOrigin.width;
+      argsMap['originHeight'] = sharePositionOrigin.height;
+    }
+
+    return argsMap;
   }
 }
